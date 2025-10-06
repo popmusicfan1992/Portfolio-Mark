3590faf76596d080601fc1c27dfd15fe84383464
MarkHuynh <popmusicfan1992@gmail.com>
2025-10-07 00:31:12 +0700

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="#"
+            href="https://drive.google.com/file/d/1dMCNAJt-tXokA_GyDoafkYL-rpZxAL0B/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
